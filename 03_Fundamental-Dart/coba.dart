ddf3199299ad9133a773894e596b258f2fe14d2d
fajrulkamal <fajrulkamal.fk2@gmail.com>
2023-08-18 16:21:57 +0700

    memindahkan tantangan.dart ke praktikum materi 3

--- 03_Fundamental-Dart/coba.dart
+++
@@ -1,4 +0,0 @@
-/* dart adalah bahasa pemrograman yang dikembangkan untuk mengatasi kekurangan yang terdapat pada javascript
-pada dart, kita bisa membuat sisi mobile, web, backend bahkan front end sehingga bisa dikatakans ebagai paket lengkap
-
-*/
