d1d9e6c8a9d4faa2102bda20ed156f5fac61dd15
fajrulkamal <fajrulkamal.fk2@gmail.com>
2023-09-23 23:44:03 +0700

    submit screenshot dan praktikum form dan layout

---
+++ 13_Advance-Form-Flutter/praktikum/praktikum/project_kontak/lib/data/kontak.dart
@@ -0,0 +1,21 @@
+import 'package:project_kontak/data/style.dart';
+
+class Dummy {
+  List<ContactModel> contacts = [
+    ContactModel(
+      id: '7a7dee66-f2f0-4dbe-9b9e-a0e8d9dedeef',
+      name: 'Humaira Anisa',
+      phoneNumber: '08123456120',
+    ),
+    ContactModel(
+      id: '884c6122-e7f2-4136-81dc-bfdedfed22a9',
+      name: 'Jiwan Kuncoro',
+      phoneNumber: '08765434098',
+    ),
+    ContactModel(
+      id: 'e8cf0dfd-242a-4fab-90fb-14d843ade4e3',
+      name: 'Keriting Burhan',
+      phoneNumber: '082344278901',
+    ),
+  ];
+}
