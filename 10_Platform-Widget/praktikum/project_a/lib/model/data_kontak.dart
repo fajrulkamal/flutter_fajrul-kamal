33f9d7ca4ee13fc767a25836c1adf3e7ae84ed97
fajrulkamal <fajrulkamal.fk2@gmail.com>
2023-09-23 22:13:01 +0700

    update

---
+++ 10_Platform-Widget/praktikum/project_a/lib/model/data_kontak.dart
@@ -0,0 +1,16 @@
+class Contact {
+  final String name;
+  final String phoneNumber;
+
+  Contact({required this.name, required this.phoneNumber});
+}
+
+List<Contact> contacts = [
+  Contact(name: 'Amanda William', phoneNumber: '123-456-7890'),
+  Contact(name: 'Jane Skywalker', phoneNumber: '987-221-3210'),
+  Contact(name: 'Hugh Jackman', phoneNumber: '765-723-1292'),
+  Contact(name: 'Sutarman', phoneNumber: '886-356-8193'),
+  Contact(name: 'Bu Khaidah', phoneNumber: '345-782-9410'),
+  Contact(name: 'Kyrgios Rodgrain', phoneNumber: '127-223-8210'),
+  Contact(name: 'Zeus Petir', phoneNumber: '673-212-1034'),
+];
