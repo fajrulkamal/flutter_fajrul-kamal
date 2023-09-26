f9c3ccd0bbed2d23deecde00253a539402ac49ad
fajrulkamal <fajrulkamal.fk2@gmail.com>
2023-09-26 16:04:57 +0700

    revisi kontak.dart

--- 12_Flutter-Form/praktikum/project_kontak/lib/data/kontak.dart
+++ 12_Flutter-Form/praktikum/project_kontak/lib/data/kontak.dart
@@ -4,18 +4,18 @@ class Dummy {
   List<ContactModel> contacts = [
     ContactModel(
       id: '7a7dee66-f2f0-4dbe-9b9e-a0e8d9dedeef',
-      name: 'Dinda Sari',
-      phoneNumber: '081234567890',
+      name: 'Humaira Anisa',
+      phoneNumber: '08123456120',
     ),
     ContactModel(
       id: '884c6122-e7f2-4136-81dc-bfdedfed22a9',
-      name: 'Budi Setiawan',
-      phoneNumber: '087654321098',
+      name: 'Jiwan Kuncoro',
+      phoneNumber: '08765434098',
     ),
     ContactModel(
       id: 'e8cf0dfd-242a-4fab-90fb-14d843ade4e3',
-      name: 'Nurul Aini',
-      phoneNumber: '082345678901',
+      name: 'Keriting Burhan',
+      phoneNumber: '082344278901',
     ),
   ];
 }
