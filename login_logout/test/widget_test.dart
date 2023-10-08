8adef6c7c2629a7f6fbe8ac0179250be74bff3ea
fajrulkamal <fajrulkamal.fk2@gmail.com>
2023-10-08 23:12:01 +0700

    pembuatan kembali folder state management provider dan submit tugas storage

--- login_logout/test/widget_test.dart
+++ login_logout/test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:utama/lib/main.dart';
+import 'package:login_logout/lib/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
